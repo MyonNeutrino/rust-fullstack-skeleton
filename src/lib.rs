5fb21d9d13d44db5eb063298a6ebdb5aeb51ba6a
Myonneutrino <christopher.fleischer1990@googlemail.com>
2023-01-29 01:40:40 +0100

    adds static files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 use axum::{routing::get, Router};
+use axum_extra::routing::SpaRouter;
 use sync_wrapper::SyncWrapper;
+use std::path::PathBuf;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_service::main]
-async fn axum() -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world));
+async fn axum(#[shuttle_static_folder::StaticFolder(folder = "public")] static_folder: PathBuf) -> shuttle_service::ShuttleAxum {
+    let router = Router::new().route("/hello", get(hello_world))
+      .merge(SpaRouter::new("/public", static_folder).index_file("index.html"))
+      ;
+
+
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
