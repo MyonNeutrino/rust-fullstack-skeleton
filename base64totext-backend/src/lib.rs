eabba8d48975c9c3f6ab70b49e270157aedd9ac6
Myonneutrino <christopher.fleischer1990@googlemail.com>
2023-01-29 04:10:07 +0100

    fixes base frontend.

--- base64totext-backend/src/lib.rs
+++ base64totext-backend/src/lib.rs
@@ -1,18 +1,52 @@
-use axum::{routing::get, Router};
-use axum_extra::routing::SpaRouter;
-use sync_wrapper::SyncWrapper;
+use axum::{routing::get, Router, body::{Body, boxed}, response::Response};
+use axum::http::StatusCode;
+use tokio::fs;
 use std::path::PathBuf;
+use sync_wrapper::SyncWrapper;
+use tower_http::services::ServeDir;
+use tower::util::ServiceExt;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_service::main]
-async fn axum(#[shuttle_static_folder::StaticFolder(folder = "public")] static_folder: PathBuf) -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/api/hello", get(hello_world))
-      .merge(SpaRouter::new("/public", static_folder).index_file("index.html"))
-      ;
+async fn axum(
+    #[shuttle_static_folder::StaticFolder(folder = "public")] static_folder: PathBuf,
+) -> shuttle_service::ShuttleAxum {
+    let router = Router::new()
+        .route("/api/hello", get(hello_world))
+        .fallback_service(get(|req| async move {
+            match ServeDir::new(&static_folder).oneshot(req).await {
+                Ok(res) => {
+                    let status = res.status();
+                    match status {
+                        StatusCode::NOT_FOUND => {
+                            let index_path = PathBuf::from(&static_folder).join("index.html");
+                            let index_content = match fs::read_to_string(index_path).await {
+                                Err(_) => {
+                                    return Response::builder()
+                                        .status(StatusCode::NOT_FOUND)
+                                        .body(boxed(Body::from("index file not found")))
+                                        .unwrap()
+                                }
+                                Ok(index_content) => index_content,
+                            };
 
+                            Response::builder()
+                                .status(StatusCode::OK)
+                                .body(boxed(Body::from(index_content)))
+                                .unwrap()
+                        }
+                        _ => res.map(boxed),
+                    }
+                }
+                Err(err) => Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(boxed(Body::from(format!("error: {err}"))))
+                    .expect("error response"),
+            }
+        }));
 
     let sync_wrapper = SyncWrapper::new(router);
 
