370600391a93e312c6a19ee02a14c99761e1fc99
Myonneutrino <christopher.fleischer1990@googlemail.com>
2023-01-30 13:08:43 +0100

    wip: adds Msg to login form.

--- base64totext-frontend/src/login.rs
+++ base64totext-frontend/src/login.rs
@@ -1,21 +1,41 @@
 use yew::{classes, html, Component, Context, Html};
 
-pub struct Login;
+pub struct Login {
+    username: String,
+    password: String,
+}
+
+pub enum LoginMsg {
+    UpdateUsername(String),
+    UpdatePassword(String),
+    Submit,
+}
 
 impl Component for Login {
-    type Message = ();
+    type Message = LoginMsg;
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
-        Self
+        Self {
+            username: "".to_string(),
+            password: "".to_string(),
+        }
     }
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        false
+        match msg {
+            LoginMsg::UpdateUsername(username) => self.username = username,
+            LoginMsg::UpdatePassword(password) => self.password = password,
+            LoginMsg::Submit => {
+                // TODO: Send the data to the server and handle the response
+            }
+        }
+        true
     }
     fn changed(&mut self, ctx: &Context<Self>, _old_props: &Self::Properties) -> bool {
         false
     }
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let onsubmit = ctx.link().callback(|_| LoginMsg::Submit);
         html! {
             <>
             <form>
@@ -27,7 +47,7 @@ impl Component for Login {
                     <label for="password">{ "Password:" }</label>
                     <input type="password" id="password" name="password"/>
                 </div>
-                <button type="submit">{"Submit"}</button>
+                <button {onsubmit} type="submit">{"Submit"}</button>
             </form>
             </>
         }
